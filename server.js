d1e7a84a2c4784fdc64332a6acb4f416fbcedda3
Jayanta <jayantadey398@gmail.com>
2021-05-02 23:55:48 -0700

    add a comment to chack last update

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ mongoose
   })
   .then(() => console.log('Database is connected successfully!'));
 
+  //Run the server
   const port = process.env.PORT || 2000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
