9283afbff1a4577919c60e49c09647f6a188bb3c
Jayanta <jayantadey398@gmail.com>
2021-07-26 07:51:23 -0700

    compalet understand query and pagination, start aggregation

--- Controller/tourController.js
+++ Controller/tourController.js
@@ -7,59 +7,13 @@ const AppError = require('./../Utils/appError');
 //Tour handler.
 
 exports.getAllTours = catchAsync(async (req, res, next) => {
-  const queryObj = { ...req.query };
-  const excludedFields = ['page', 'sort', 'limit', 'fields'];
-  excludedFields.forEach((el) => delete queryObj[el]);
-
-  //Advanded filtering
-  let queryStr = JSON.stringify(queryObj);
-  queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
-  console.log(JSON.parse(queryStr));
-  let query = Tour.find(JSON.parse(queryStr));
-
-  if(req.query.sort){
-    const sortBy = req.query.sort.split(',').join(' ');
-    query = query.sort(sortBy)
-  } else{
-    query = query.sort('-createdAt')
-  }
-
-  if(req.query.fields){
-    const fields = req.query.fields.split(',').join(' ');
-    query = query.select(fields);
-  }else{
-    query = query.select('-__v')
-  }
-
-
-//Pagination
-const page = req.query.page * 1 || 1;
-// console.log(page)
-
-const limit = req.query.limit * 1 || 10;
-console.log(limit)
-
-const skip = (page - 1) * limit; 
-console.log('skip',skip)
-
-query = query.skip(skip).limit(limit)
-
-if(req.query.page){
-  const docNum = await Tour.countDocuments();
-  if(skip >= docNum) throw new Error('This page is not exist!')
-}
-
-
-
-
-  //Execute query
-  const tours = await query;
-  // const features = new APIFeatures(Tour.find(), req.query)
-  //   .filter()
-  //   .sort()
-  //   .limitFields()
-  //   .paginate();
-  // const tours = await features.query;
+  //Execute query  
+  const features = new APIFeatures(Tour.find(), req.query)
+    .filter()
+    .sort()
+    .limitFields()
+    .paginate();
+  const tours = await features.query;
 
   res.status(200).json({
     status: 'success',
