2bdf2921cb0135ed0b5743f978d470c2b7429709
Jayanta <jayantadey398@gmail.com>
2021-08-05 14:03:09 -0700

    understanding unhandel Errors

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 //Start the server..
 const dotenv = require('dotenv');
+
+//Unhandle Exception
+process.on('uncaughtException', err =>{
+  console.log('UNHANDLED EXCEPTION! I am Shuttion down...')
+  console.log(err.name, err.message); 
+})
+
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 const mongoose = require('mongoose');
@@ -17,10 +24,19 @@ mongoose
     useUnifiedTopology: true,
   })
   .then(() => console.log('Database is connected successfully!'))
-  .catch((err) => console.log('somthing is wrong DB is not conocted', err));
+  // .catch((err) => console.log('somthing is wrong DB is not conocted', err));
 
 //Run the server in port 2000
 const port = process.env.PORT || 3002;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
+
+//Unhandle Errors
+process.on('unhandledRejection', err =>{
+  console.log('UNHANDLED REJECTION! I am Shuttion down...')
+  console.log(err.name, err.message);
+  server.close(()=>{
+    process.exit(1);
+  })
+})
