c95a8d538e4e347d3a72ebd4ff0a38168115529c
Jayanta <jayantadey398@gmail.com>
2021-08-15 09:43:28 -0700

    implementing current user update route

--- Controller/userController.js
+++ Controller/userController.js
@@ -3,6 +3,14 @@ const AppError = require('../Utils/appError');
 const catchAsync = require('../Utils/catchAsync');
 const jwt = require('jsonwebtoken');
 
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
+
 //JWT Token
 const signToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET_TOKEN, {
@@ -41,7 +49,7 @@ exports.login = catchAsync(async (req, res, next) => {
   creactSendToken(user, 200, res);
 });
 
-exports.getUser = catchAsync(async (req, res) => {
+exports.getUser = catchAsync(async (req, res, next) => {
   const user = await User.find();
   creactSendToken(user, 200, res);
 });
@@ -52,12 +60,30 @@ exports.getUserById = catchAsync(async (req, res, next) => {
   creactSendToken(user, 200, res);
 });
 
-exports.updateUser = (req, res) => {
-  res.status(500).json({
-    status: 'err',
-    message: 'This rout is not yet defined',
+//Update the curent user
+exports.updateMe = catchAsync(async (req, res, next) => {
+  //Send err if user update password or confirmPassword
+  if (req.body.password || req.body.passwordConfarmation) {
+    return next(
+      new AppError(
+        'This is not update password route. Please viset /updatePassword route',
+        400
+      )
+    );
+  }
+  //if not then update the user
+  const filterdBody = filterObj(req.body, 'name', 'email');
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filterdBody, {
+    new: true,
+    runValidators: true,
   });
-};
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser,
+    },
+  });
+});
 
 exports.deleteUser = (req, res) => {
   res.status(500).json({
--- Routs/userRouts.js
+++ Routs/userRouts.js
@@ -11,18 +11,36 @@ router.post('/login', userController.login);
 //forgot Password
 router.post('/forgotPassword', AuthController.forgotPassword);
 router.patch('/resetPassword/:token', AuthController.resetPassword);
-router.patch('/updatePassword', AuthController.protectRoute, AuthController.updatePassword)
+router.patch(
+  '/updatePassword',
+  AuthController.protectRoute,
+  AuthController.updatePassword
+);
 
 //user route
 router
   .route('/')
-  .get(AuthController.protectRoute, AuthController.restrictTo('admin', 'lead-guide'), userController.getUser)
+  .get(
+    AuthController.protectRoute,
+    AuthController.restrictTo('admin', 'lead-guide'),
+    userController.getUser
+  );
+
+router.patch('/updateMe', AuthController.protectRoute, userController.updateMe);
 
 // Get User by ID
 router
   .route('/:id')
-  .get(AuthController.protectRoute,AuthController.restrictTo('admin', 'lead-guide'), userController.getUserById)
-  .patch(AuthController.protectRoute, userController.updateUser)
-  .delete(AuthController.protectRoute, AuthController.restrictTo('admin', 'lead-guide'), userController.deleteUser);
+  .get(
+    AuthController.protectRoute,
+    AuthController.restrictTo('admin', 'lead-guide'),
+    userController.getUserById
+  )
+  // .patch(AuthController.protectRoute, userController.updateUser)
+  .delete(
+    AuthController.protectRoute,
+    AuthController.restrictTo('admin', 'lead-guide'),
+    userController.deleteUser
+  );
 
 module.exports = router;
